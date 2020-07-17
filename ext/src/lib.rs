e365edf325bb17612bbffa0b27aaec90fed313a4
Markus Thömmes <markusthoemmes@me.com>
2020-07-17 16:49:48 +0200

    Implementing a 'SimplePod' controller.
    This implements a... well... SimplePod CRD that only takes an images and creates a very basic pod with that image. It also does basic reconcilation of the created object.

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -1,23 +1,90 @@
 use k8s_openapi::api::core::v1::Pod;
-use kube::api::ListParams;
-use kube::runtime::Reflector;
+use kube::api::{ListParams, Meta, PostParams, WatchEvent};
+use kube::runtime::Informer;
 use kube::{Api, Client};
+
+use kube_derive::CustomResource;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+#[derive(CustomResource, Deserialize, Serialize, Clone, Debug)]
+#[kube(group = "slinky.dev", version = "v1", namespaced)]
+pub struct SimplePodSpec {
+    image: String,
+}
+
+// TODO: Add status?
 
 #[no_mangle]
 pub extern "C" fn run() {
     let client = Client::default();
 
-    let pods: Api<Pod> = Api::namespaced(client, "default");
-    let lp = ListParams::default().timeout(1);
-    let rf = Reflector::new(pods).params(lp);
+    let foos: Api<SimplePod> = Api::namespaced(client.clone(), "default");
+    let inform = Informer::new(foos).params(ListParams::default().timeout(1));
+
+    let pods: Api<Pod> = Api::namespaced(client.clone(), "default");
 
     loop {
-        rf.poll().expect("Poll error!");
+        let events = inform.poll().expect("poll error");
 
-        println!("Poll completed, pods in default:");
-
-        for p in rf.state().expect("Cannot get reflector state") {
-            println!("{:?}", p)
+        for event in events {
+            let e = event.expect("event error");
+            match e {
+                WatchEvent::Added(o) => {
+                    reconcile_pod(&pods, &o.name(), &o.spec.image).expect("reconcile error");
+                }
+                WatchEvent::Modified(o) => {
+                    reconcile_pod(&pods, &o.name(), &o.spec.image).expect("reconcile error");
+                }
+                WatchEvent::Error(e) => {
+                    println!("Error event: {:?}", e);
+                }
+                _ => {}
+            }
         }
     }
+}
+
+fn reconcile_pod(pods: &Api<Pod>, name: &str, image: &str) -> Result<Pod, kube::Error> {
+    match pods.get(&name) {
+        Ok(mut existing) => {
+            let existing_image = existing
+                .spec
+                .as_ref()
+                .map(|spec| spec.containers[0].image.as_ref())
+                .flatten()
+                .expect("this should never happen");
+            if existing_image == image {
+                println!("image is equal, doing nothing");
+                Ok(existing)
+            } else {
+                let mut spec = existing.spec.unwrap();
+                spec.containers[0].image = Some(image.to_string());
+                existing.spec = Some(spec);
+                println!("replacing pod");
+                pods.replace(&existing.name(), &PostParams::default(), &existing)
+            }
+        }
+        Err(kube::Error::Api(ae)) if ae.code == 404 => {
+            println!("creating pod");
+            pods.create(&PostParams::default(), &pod(name, image))
+        }
+        e => e,
+    }
+}
+
+fn pod(name: &str, image: &str) -> Pod {
+    // TODO: Add ownerRef for deletion handling.
+    return serde_json::from_value(json!({
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": { "name": name },
+        "spec": {
+            "containers": [{
+              "name": "default-container",
+              "image": image
+            }],
+        }
+    }))
+    .unwrap();
 }
